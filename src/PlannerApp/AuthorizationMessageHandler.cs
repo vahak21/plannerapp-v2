af372e650fb1542eea47af7701c68b1bee3db036
Vahakn Gharibian <ken@thevgroup.com>
2024-02-20 16:04:53 -0800

    Added Message Handler for HTTP Client

---
+++ src/PlannerApp/AuthorizationMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using Blazored.LocalStorage;
+
+public class AuthorizationMessageHandler  : DelegatingHandler
+{
+    private readonly ILocalStorageService _storage;
+    public AuthorizationMessageHandler(ILocalStorageService storage)
+    {
+        _storage = storage;
+    }
+
+    protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if ( await _storage.ContainKeyAsync("access_token"))
+        {
+            var token = await _storage.GetItemAsStringAsync("access_token");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+        Console.WriteLine("Authorization Message handler Called.");
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,8 +8,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("PlannerApp.Api", client =>
+{
+    client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+builder.Services.AddTransient<AuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
 
 builder.Services.AddMudServices();
-
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
