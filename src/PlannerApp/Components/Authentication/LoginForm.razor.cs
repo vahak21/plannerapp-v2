865f3cae11502546a9a759cb286d7e922b6890ee
Ken Gharibian <Ken@thevgroup.com>
2024-02-20 23:20:50 -0800

    Create Authentication Layour and login form

---
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Shared.Models;
+
+namespace PlannerApp.Components
+{
+    public partial class LoginForm : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+        private LoginRequest _model = new LoginRequest();
+
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
